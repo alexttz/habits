360060567f1d541e102d8fe52e6c8f6c245dfaeb
Alexander <lima.alexander.las@gmail.com>
2023-01-24 16:47:17 -0300

    First commit

---
+++ habits/script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function save() {
+
+  //Salvando as alterações do formulário e transformando em Strings
+localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+function add() {
+    //Formatando a data atual e salvando na variável
+  const today = "27/01"/*new Date().toLocaleDateString('pt-br').slice(0,-5)*/
+    //Chamando a função 'dayExist' e armazenando na variável
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists){
+    alert("Dia já está incluso 🚨")
+    return
+  }
+
+  nlwSetup.addDay(today)
+  alert("Adicionado com sucesso! ✅")
+}
+
+//Retirando as informações salvas do formulário e transformando em objetos
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+
+nlwSetup.setData(data)
+nlwSetup.load()
